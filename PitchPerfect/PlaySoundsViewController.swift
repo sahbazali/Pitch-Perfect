3aabd6af1055743fa894662fb751b105753e8e81
ali-sahbaz <ali.sahbaz@arcelik.com>
2020-11-29 23:42:02 +0300

    Prevent button images squishing.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -31,10 +31,20 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAudio()
+        preventSquishingButtonImages()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         configureUI(.notPlaying)
+    }
+    
+    func preventSquishingButtonImages(){
+        snailButton.imageView?.contentMode = .scaleAspectFit
+        chipmunkButton.imageView?.contentMode = .scaleAspectFit
+        rabbitButton.imageView?.contentMode = .scaleAspectFit
+        vaderButton.imageView?.contentMode = .scaleAspectFit
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        reverbButton.imageView?.contentMode = .scaleAspectFit
     }
     
     // MARK: Actions
